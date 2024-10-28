19e047d505e0c640b6e07f5e0da0e9f72aaed72b
DipanshuVartak <94838472+DipanshuVartak@users.noreply.github.com>
2024-10-28 13:23:16 +0000

    config done

---
+++ 01_basics/02_dt.js
@@ -0,0 +1,13 @@
+console.log("hello")
+const id = 123
+let name = "John"
+let age = 10
+let isLoggedin = false
+surname = "Wick";
+let state
+isLoggedout = false
+console.log(isLoggedout)
+console.log(surname);
+console.table([id,name,age,surname,isLoggedin,state])
+console.log(typeof state)
+console.log(typeof null)
